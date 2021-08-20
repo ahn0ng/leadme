ee8ccabc49e7d4deaa6bd877a9f66356dc794813
ahn0ng <71095151+ahn0ng@users.noreply.github.com>
2021-08-20 13:22:09 +0900

    Add files via upload

---
+++ map.js
@@ -0,0 +1,56 @@
+$(function(){
+
+    var $side_menu =$("#side_btn li");
+    var $con = $("#side_con > ul > li");
+
+    $side_menu.eq(0).click(function(){
+    $("#side_con").find($con.eq(0)).stop().animate({"margin-left":"0"},500,"linear")
+    $("#side_con").find($con.eq(1)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(2)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(3)).stop().animate({"margin-left":"400"},1,"linear");
+    });
+
+$side_menu.eq(1).click(function(){
+    $("#side_con").find($con.eq(1)).stop().animate({"margin-left":"0"},500,"linear")
+    $("#side_con").find($con.eq(0)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(2)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(3)).stop().animate({"margin-left":"400"},1,"linear");
+    });
+
+$side_menu.eq(2).click(function(){
+    $("#side_con").find($con.eq(2)).stop().animate({"margin-left":"0"},500,"linear")
+    $("#side_con").find($con.eq(0)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(1)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(3)).stop().animate({"margin-left":"400"},1,"linear");
+    });
+
+$side_menu.eq(3).click(function(){
+    $("#side_con").find($con.eq(3)).stop().animate({"margin-left":"0"},500,"linear")
+    $("#side_con").find($con.eq(0)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(1)).stop().animate({"margin-left":"400"},1,"linear")
+    $("#side_con").find($con.eq(2)).stop().animate({"margin-left":"400"},1,"linear");
+    });
+});
+
+$(function(){
+
+$("#imp_f").mouseenter(function(){
+    
+    $("#imp_menu").fadeIn();
+})
+$("#imp_f").mouseleave(function(){
+
+    $("#imp_menu").fadeOut();
+});
+
+});
+
+const add_textbox = () => {
+    const adding = document.getElementById("place");
+    const newP = document.createElement('p');
+    newP.innerHTML = '<input type="search" class="place2"><span id="delete" onclick="remove(this)">-</p>';
+    adding.appendChild(newP);
+}
+const remove = (obj) => {
+    document.getElementById('place').removeChild(obj.parentNode);
+}
